94d43f7611402cfc4d0b0e69f86c16c8985cd35c
Javier Guerra <javierguerradevelop@gmail.com>
2025-12-28 21:44:49 -0500

    line_reader.h structure based off the WoW combat log. minor changes to clang format and cmakelists.

---
+++ include/parser.h
@@ -0,0 +1,20 @@
+#ifndef SHOTCALLERCPP_PARSER_H
+#define SHOTCALLERCPP_PARSER_H
+
+#include <chrono>
+#include <string>
+
+struct CombatEvent {
+    std::chrono::system_clock::time_point time_stamp;
+    std::string event_type;
+    std::string name;
+    std::string source_id;
+    std::string target_id;
+    std::string source_raid_flag;
+    std::string spell_name;
+    int spell_id;
+};
+
+CombatEvent parse_line(const std::string& string);
+
+#endif  // SHOTCALLERCPP_PARSER_H
